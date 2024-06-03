5849e6ffa141f8932d5a29b1b62757c75a897e1c
Ermia Ghaffari <ermia@kth.se>
2024-06-03 11:46:26 +0200

    Added all the comments for communication  diagram 1

--- Exc/src/main/java/se/kth/iv1350/ermia/view/View.java
+++ Exc/src/main/java/se/kth/iv1350/ermia/view/View.java
@@ -1,3 +1,7 @@
+/**
+ * The <code>View</code> class is the hard-coded part of the user interface.
+ * It interacts with the controller to perform operations and displays the results at the end.
+ */
 package se.kth.iv1350.ermia.view;
 
 import se.kth.iv1350.ermia.controller.Controller;
@@ -7,9 +11,18 @@ import java.util.Random;
 
 public class View {
     private Controller cntr;
+    /**
+     * The constructor which creates a new instance of <code>View</code>.
+     *
+     * @param cntr The controller that is used for handling user interactions.
+     */
     public View(Controller cntr){
         this.cntr = cntr;
     }
+    /**
+     * Runs a sample scenario of the application
+     * It adds items to a sale and displaying the sale details
+     */
     public void runSample(){
         Random random = new Random();
         cntr.startSale();
@@ -17,6 +30,5 @@ public class View {
         saleDTO = cntr.addItem(101, 4);
         saleDTO = cntr.addItem(102, 2);
         System.out.println(saleDTO);
-
     }
 }
